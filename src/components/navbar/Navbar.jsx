b9f260a4daa11c8307376e9841e149fb58227c69
Namig <namig.pashayev1@gmail.com>
2025-07-17 14:40:47 +0400

    Improve navbar structure

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,24 +3,26 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav>
+    <header>
       <div className={styles.left}>
         <img className={styles.logo} src="/logo.png" alt="logo" />
         <div className={styles.name}>Online Shop</div>
       </div>
 
-      <ul className={styles.linksContainer}>
-        <li>
-          <Link to="/" className={styles.link}>
-            Home
-          </Link>
+      <nav>
+        <ul className={styles.linksContainer}>
+          <li>
+            <Link to="/" className={styles.link}>
+              Home
+            </Link>
 
-          <Link to="/products" className={styles.link}>
-            Products
-          </Link>
-        </li>
-      </ul>
-    </nav>
+            <Link to="/products" className={styles.link}>
+              Products
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
