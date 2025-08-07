0bfe9c47c319764c649c95410ece48a6874f093a
Namig <namig.pashayev1@gmail.com>
2025-08-07 14:45:07 +0400

    Create reusable api client

---
+++ src/services/apiClient.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+    baseURL: 'https://dummyjson.com',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+
+apiClient.interceptors.request.use(
+    (config) => {
+        return config;
+    },
+
+    (error) => {
+        return Promise.reject(error);
+    }
+)
+
+
+apiClient.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+
+    (error) => {
+        return Promise.reject(error);
+    }
+)
+
+export default apiClient;
