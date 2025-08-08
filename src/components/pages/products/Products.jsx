e739dc2561861e81e1a42f2500da35e170c05e76
Namig <namig.pashayev1@gmail.com>
2025-08-08 12:31:51 +0400

    Refactor the structure of ProductsPage

--- src/components/pages/products/Products.jsx
+++ src/components/pages/products/Products.jsx
@@ -1,80 +1,68 @@
-import { useProducts } from '../../../hooks/useProducts'
 import styles from './products.module.scss'
 import Loading from '../../Loading'
 import FilterBar from './FilterBar'
-import { useEffect, useState } from 'react'
 
 
-const Products = () => {
-    const [url, setUrl] = useState('/products')
-    const [params, setParams] = useState({})
+const Products = ({ productsData }) => {
 
-    const { data: products, isLoading, error } = useProducts(url, params)
+    const { data: products, isLoading, error } = productsData;
 
-    useEffect(() => console.log(params, url, products?.length), [params, url, products?.length])
-
+    if (isLoading) return <Loading size='30px' />
     if (error) return <main>An error occurred while fetching products</main>
 
     return (
         <main>
-            <FilterBar
-                setUrl={setUrl}
-                params={params}
-                setParams={setParams}
-            />
-
             <div className={styles.productsCnr}>
                 {
-                    isLoading ? <Loading size={'30px'} /> :
-                        products?.length > 0 ? (
-                            products.map(product => {
+                    products?.length > 0 ? (
+                        products.map(product => {
 
-                                const discountPercentage = Math.floor(product.discountPercentage);
+                            const discountPercentage = Math.floor(product.discountPercentage);
 
-                                return (
-                                    <div key={product.id} className={styles.product}>
-                                        <p className={styles.brand}>{product.brand}</p>
-                                        {
-                                            discountPercentage > 0 &&
-                                            <div className={styles.discountPercentage}>
-                                                -{discountPercentage}%
-                                            </div>
-                                        }
-                                        <div className={styles.imageCnr}>
-                                            <img
-                                                src={product.images[0]}
-                                                className={styles.image}
-                                                alt={product.title}
-                                            />
+                            return (
+                                <div key={product.id} className={styles.product}>
+                                    <p className={styles.brand}>{product.brand}</p>
+                                    {
+                                        discountPercentage > 0 &&
+                                        <div className={styles.discountPercentage}>
+                                            -{discountPercentage}%
                                         </div>
-                                        <div className={styles.info}>
-                                            <div className={styles.titleCnr}>
-                                                <p className={styles.title}>{product.title}</p>
-                                                <div className={styles.ratingCnr}>
-                                                    <div className={styles.rating}>{(product.rating).toFixed(1)}</div>
-                                                    <div className={styles.starsCnr} style={{ width: `${(Number(product.rating.toFixed(1)) / 5) * 60}px` }}>
-                                                        <img src="/src/assets/star.png" alt="" className={styles.star} />
-                                                        <img src="/src/assets/star.png" alt="" className={styles.star} />
-                                                        <img src="/src/assets/star.png" alt="" className={styles.star} />
-                                                        <img src="/src/assets/star.png" alt="" className={styles.star} />
-                                                        <img src="/src/assets/star.png" alt="" className={styles.star} />
-                                                    </div>
+                                    }
+                                    <div className={styles.imageCnr}>
+                                        <img
+                                            src={product.images[0]}
+                                            className={styles.image}
+                                            alt={product.title}
+                                        />
+                                    </div>
+                                    <div className={styles.info}>
+                                        <div className={styles.titleCnr}>
+                                            <p className={styles.title}>{product.title}</p>
+                                            <div className={styles.ratingCnr}>
+                                                <div className={styles.rating}>{(product.rating).toFixed(1)}</div>
+                                                <div className={styles.starsCnr} style={{ width: `${(Number(product.rating.toFixed(1)) / 5) * 60}px` }}>
+                                                    <img src="/src/assets/star.png" alt="" className={styles.star} />
+                                                    <img src="/src/assets/star.png" alt="" className={styles.star} />
+                                                    <img src="/src/assets/star.png" alt="" className={styles.star} />
+                                                    <img src="/src/assets/star.png" alt="" className={styles.star} />
+                                                    <img src="/src/assets/star.png" alt="" className={styles.star} />
                                                 </div>
                                             </div>
+                                        </div>
 
-                                            <div className={styles.priceCnr}>
-                                                {discountPercentage > 0 &&
-                                                    <p className={styles.oldPrice}>
-                                                        {(product.price / (1 - discountPercentage / 100)).toFixed(2)}$
-                                                    </p>
-                                                }
-                                                <p className={styles.newPrice}>{product.price}$</p>
-                                            </div>
+                                        <div className={styles.priceCnr}>
+                                            {discountPercentage > 0 &&
+                                                <p className={styles.oldPrice}>
+                                                    {(product.price / (1 - discountPercentage / 100)).toFixed(2)}$
+                                                </p>
+                                            }
+                                            <p className={styles.newPrice}>{product.price}$</p>
                                         </div>
                                     </div>
-                                )
-                            })
-                        ) : <p>No products found</p>
+                                </div>
+                            )
+                        })
+                    ) : <p>No products found</p>
                 }
             </div>
 
--- src/components/pages/products/ProductsPage.jsx
+++ src/components/pages/products/ProductsPage.jsx
@@ -1,10 +1,28 @@
+import styles from './products-page.module.scss'
 import Products from "./Products";
+import Sidebar from "./Sidebar";
+import FilterBar from './FilterBar';
+import { useState } from 'react';
+import { useProducts } from '../../../hooks/useProducts';
+
 
 const ProductsPage = () => {
+    const [url, setUrl] = useState('/products')
+    const [params, setParams] = useState({ q: '' })
+
+    const productsData = useProducts(url, params)
 
     return (
-        <div>
-            <Products />
+        <div className={styles.pageContainer}>
+            <FilterBar
+                setUrl={setUrl}
+                params={params}
+                setParams={setParams}
+            />
+            <div className={styles.main}>
+                <Sidebar />
+                <Products productsData={productsData} />
+            </div>
         </div>
     )
 }
