bd52f2ccdbe05e0ba66f6259baac0acaffb2b382
Namig <namig.pashayev1@gmail.com>
2025-08-08 12:30:19 +0400

    Add category-list fetching service and query

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,9 +1,16 @@
 import { useQuery } from '@tanstack/react-query'
-import { getProducts } from '../services/productService';
+import { getCategories, getProducts } from '../services/productService';
 
 export const useProducts = (url, params) => {
     return useQuery({
         queryKey: ['products', params],
         queryFn: () => getProducts(url, params)
+    })
+}
+
+export const useCategories = () => {
+    return useQuery({
+        queryKey: ['categories'],
+        queryFn: getCategories
     })
 }
--- src/services/productService.js
+++ src/services/productService.js
@@ -4,3 +4,8 @@ export const getProducts = (url, params) =>
     apiClient
         .get(url, { params })
         .then(res => res.data.products);
+
+export const getCategories = () =>
+    apiClient
+        .get('/products/categories')
+        .then(res => res.data)
