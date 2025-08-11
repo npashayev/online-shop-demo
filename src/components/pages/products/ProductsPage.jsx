6969000ad73fefd987a5b35c23039ff0e20e5bba
Namig <namig.pashayev1@gmail.com>
2025-08-11 16:22:28 +0400

    Update browserRouter and ProductsPage to render the routed components

--- src/components/pages/products/ProductsPage.jsx
+++ src/components/pages/products/ProductsPage.jsx
@@ -1,28 +1,17 @@
 import styles from './products-page.module.scss'
-import Products from "./Products";
 import Sidebar from "./Sidebar";
 import FilterBar from './FilterBar';
-import { useState, useEffect } from 'react';
-import { useProducts } from '../../../hooks/useProducts';
+import { Outlet } from 'react-router-dom';
 
 
 const ProductsPage = () => {
-    const [url, setUrl] = useState('/products')
-    const [params, setParams] = useState({ q: '' })
-
-    const productsData = useProducts(url, params)
-    useEffect(() => console.log(url), [url])
 
     return (
         <div className={styles.pageContainer}>
-            <FilterBar
-                setUrl={setUrl}
-                params={params}
-                setParams={setParams}
-            />
+            <FilterBar />
             <div className={styles.main}>
-                <Sidebar url={url} setUrl={setUrl} />
-                <Products productsData={productsData} />
+                <Sidebar />
+                <Outlet />
             </div>
         </div>
     )
--- src/routing/routes.jsx
+++ src/routing/routes.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../components/pages/home/HomePage";
 import ProductsPage from "../components/pages/products/ProductsPage";
 import Layout from "../components/pages/Layout";
+import AllProducts from "../components/pages/products/AllProducts";
+import SearchProducts from "../components/pages/products/SearchProducts";
 
 
 const router = createBrowserRouter([
@@ -10,7 +12,14 @@ const router = createBrowserRouter([
         element: <Layout />,
         children: [
             { index: true, element: <HomePage /> },
-            { path: 'products', element: <ProductsPage /> },
+            {
+                path: 'products',
+                element: <ProductsPage />,
+                children: [
+                    { index: true, element: <AllProducts /> },
+                    { path: 'search', element: <SearchProducts /> }
+                ]
+            },
         ]
     }
 ]);
