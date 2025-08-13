460818367b3060f40d8392c894210b79b3b8a668
Namig <namig.pashayev1@gmail.com>
2025-08-13 15:47:53 +0400

    Update browserRouter

--- src/routing/routes.jsx
+++ src/routing/routes.jsx
@@ -5,6 +5,7 @@ import Layout from "../components/pages/Layout";
 import AllProducts from "../components/pages/products/AllProducts";
 import SearchProducts from "../components/pages/products/SearchProducts";
 import ProductsByCategory from "../components/pages/products/ProductsByCategory";
+import ProductDetails from "../components/pages/productDetails/ProductDetailsPage";
 
 
 const router = createBrowserRouter([
@@ -22,6 +23,10 @@ const router = createBrowserRouter([
                     { path: 'category/:category', element: <ProductsByCategory /> }
                 ]
             },
+            {
+                path: 'products/:id',
+                element: <ProductDetails />
+            }
         ]
     }
 ]);
