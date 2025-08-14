e1c57446e27912e736d4e8d9f11a6cd4e64e6986
Namig <namig.pashayev1@gmail.com>
2025-08-14 11:45:11 +0400

    Create ProductDetailsPage component for showing individual product data

--- src/components/pages/productDetails/ProductDetailsPage.jsx
+++ src/components/pages/productDetails/ProductDetailsPage.jsx
@@ -1,7 +1,28 @@
+import { useProductById } from "../../../hooks/useProducts"
+import { useParams } from "react-router-dom"
+import styles from './product-details-page.module.scss'
+import Loading from "../../common/Loading"
 
 const ProductDetailsPage = () => {
+
+    const { id } = useParams();
+
+    const { data: product, isLoading, error } = useProductById(id);
+
+    if (error) return <div>An error occurred while fetching product</div>
+
+    if (isLoading) return <Loading size="50px" />
+
     return (
-        <div>ProductDetailsPage</div>
+        product &&
+        <main className={styles.pageContainer}>
+            <div className={styles.categoryContainer}>
+                Category / <span className={styles.categoryName}>{product.category}</span>
+            </div>
+
+            <div className={styles.mainInfo}>
+            </div>
+        </main>
     )
 }
 
