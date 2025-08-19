3d35f92bbcfe1ebd0ff5d33e296de4e101b0c80b
Namig <namig.pashayev1@gmail.com>
2025-08-19 11:39:58 +0400

    Store token on login and automatically add Bearer token to requests

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,9 +1,20 @@
 import { useMutation } from "@tanstack/react-query";
 import { login } from "../services/authService";
+import { useDispatch } from "react-redux";
+import { setUser } from "../store/userSlice";
 
 
-export const useLogin = () =>
-    useMutation({
+export const useLogin = () => {
+    const dispatch = useDispatch();
+
+    return useMutation({
         mutationFn: (data) => login(data),
-        onSuccess: (data) => console.log(data)
+        onSuccess: (data) => {
+            dispatch(setUser(data));
+
+            //store tokens in local storage
+            localStorage.setItem("accessToken", data.accessToken)
+            localStorage.setItem("refreshToken", data.refreshToken)
+        }
     })
+}
--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -9,6 +9,10 @@ const apiClient = axios.create({
 
 apiClient.interceptors.request.use(
     (config) => {
+        const token = localStorage.getItem("accessToken");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
         return config;
     },
 
