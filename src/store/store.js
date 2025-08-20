1ca4b0220822bcf0916c6016487b602b7ce75b44
Namig <namig.pashayev1@gmail.com>
2025-08-20 11:15:43 +0400

    Fix: store only serializable data in Redux persist to avoid warnings

--- src/store/store.js
+++ src/store/store.js
@@ -1,23 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userReducer from './userSlice';
-import storage from "redux-persist/lib/storage";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { persistReducer, persistStore } from "redux-persist";
-import { combineReducers } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import userReducer from './userSlice';
 
 const persistConfig = {
     key: "root",
     storage,
-    whitelist: ["user"]
-}
+    whitelist: ["user"] // only persist serializable user slice
+};
 
 const rootReducer = combineReducers({
     user: userReducer
-})
+});
 
+// Persist only the reducer, never any hooks/functions
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: persistedReducer
-})
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE']
+            }
+        })
+});
 
 export const persistor = persistStore(store);
