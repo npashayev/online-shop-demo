94a4a5f8d22cdad92c16ad38e72a796f1bb8a923
Namig <namig.pashayev1@gmail.com>
2025-08-26 15:05:33 +0400

    Create CartDropdown component

---
+++ src/components/common/navbar/cart-dropdown/CartDropdown.jsx
@@ -0,0 +1,11 @@
+import styles from './cart-dropdown.module.scss'
+
+const CartDropdown = () => {
+    return (
+        <div className={styles.main}>
+
+        </div>
+    )
+}
+
+export default CartDropdown
