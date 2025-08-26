4b2b575f361632cfad17ae88c820683c44276b22
Namig <namig.pashayev1@gmail.com>
2025-08-26 10:54:06 +0400

    Render Not authorized component if the user want to enter the data of other user

--- src/components/pages/PrivateRoutes.jsx
+++ src/components/pages/PrivateRoutes.jsx
@@ -1,10 +1,18 @@
+import NotAuthorized from './not-authorized/NotAuthorized';
 import useAuth from '/src/hooks/useAuth'
-import { Navigate, Outlet } from 'react-router-dom'
+import { Navigate, Outlet, useParams } from 'react-router-dom'
 
 const PrivateRoutes = () => {
-    const { isAuthenticated } = useAuth();
+    const { isAuthenticated, user } = useAuth();
+
+    const { id } = useParams();
+
 
     if (!isAuthenticated) return <Navigate to="/login" replace />
+
+    if (id !== String(user.id)) {
+        return <NotAuthorized />;
+    }
 
     return <Outlet />
 }
