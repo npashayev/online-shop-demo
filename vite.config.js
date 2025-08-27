5dda7d382e561c7fcb472afca8c4ee74bc577d22
Namig <namig.pashayev1@gmail.com>
2025-08-27 09:03:43 +0400

    Update vite config file

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,9 @@ export default defineConfig({
       components: path.resolve(__dirname, 'src', 'components'),
       pages: path.resolve(__dirname, 'src', 'components', 'pages'),
       assets: path.resolve(__dirname, 'src', 'assets'),
-      utils: path.resolve(__dirname, 'src', 'utils'), // <- fixed
+      utils: path.resolve(__dirname, 'src', 'utils'),
+      store: path.resolve(__dirname, 'src', 'store'),
+      services: path.resolve(__dirname, 'src', 'services'),
     },
   },
 });
