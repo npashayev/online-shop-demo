9489caa1d6498dfd25c82bd59c892634d2adea28
Namig <namig.pashayev1@gmail.com>
2025-08-27 09:29:19 +0400

    Update cartService

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -4,4 +4,4 @@ import apiClient from "./apiClient";
 export const getUserCarts = (id) =>
     apiClient
         .get(`/users/${id}/carts`)
-        .then(res => res.data?.carts[0])
+        .then(res => res.data)
