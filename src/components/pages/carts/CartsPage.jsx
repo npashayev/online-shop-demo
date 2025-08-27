7fcc04ea2fa56461f361faf994e2918e07d8452c
Namig <namig.pashayev1@gmail.com>
2025-08-27 10:40:46 +0400

    Render fetched carts in the CartsPage

--- src/components/pages/carts/CartsPage.jsx
+++ src/components/pages/carts/CartsPage.jsx
@@ -2,45 +2,73 @@ import { useUserCarts } from 'hooks/useUserCarts'
 import styles from './carts.module.scss'
 import useAuth from 'hooks/useAuth'
 import Loading from 'components/common/Loading';
+import { useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleMinus, faCirclePlus, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
 const CartsPage = () => {
 
     const { user } = useAuth();
 
-    const { data: cart, isLoading, error } = useUserCarts(user.id)
+    const { data: cartsData, isLoading, error } = useUserCarts(user.id)
 
+    // useEffect(() => console.log(cartsData), [cartsData])
     if (error) return <div className={styles.main}>Error happened while fetching cart</div>
+
 
     if (isLoading) return <div className={styles.main}><Loading /></div>
 
     return (
-        <div className={styles.main}>
-            <div className={styles.cart}>
-                {
-                    (cart && cart.products.length > 0)
-                        ? cart.products.map(product =>
-                            <div
-                                key={product.id}
-                                className={styles.cartItem}
-                            >
-                                <div className={styles.imageCnr}>
-                                    <img src={product.thumbnail} title={product.title} className={styles.thumbnail} />
-                                </div>
-
-                                <div className={styles.infoCnr}>
-                                    <div className={styles.title}>
-                                        {product.title}
-                                    </div>
-
-                                    <div className={styles.title}>
-                                    </div>
-                                </div>
-                            </div>
-                        )
-                        : <div>There's no products in the basket</div>
-                }
+        <main className={styles.main}>
+            <div className={styles.mainHeading}>
+                <div className={styles.headingText}>You have {cartsData.total} carts in your basket</div>
             </div>
-        </div>
+
+            {
+                cartsData.carts.map((cart, i) =>
+                    <div key={cart.id} className={styles.cart}>
+                        <div className={styles.cartHeader}>
+                            Cart {i + 1}
+                        </div>
+
+                        <div className={styles.productsCnr}>
+                            {
+                                cart.products.map(product =>
+                                    <div key={product.id} className={styles.product}>
+                                        <div className={styles.imageCnr}>
+                                            <img src={product.thumbnail} className={styles.productImage} />
+                                        </div>
+
+                                        <div className={styles.orderInfo}>
+                                            <div className={styles.productTitle}>{product.title}</div>
+                                            <div className={styles.productQuantityCnr}>
+                                                <FontAwesomeIcon icon={faCircleMinus} className={styles.counterIcon} />
+                                                <div className={styles.productQuantity}>
+                                                    {product.quantity}
+                                                </div>
+                                                <FontAwesomeIcon icon={faCirclePlus} className={styles.counterIcon} />
+                                            </div>
+                                        </div>
+
+                                        <div className={styles.totalPrice}>
+                                            ${product.discountedTotal.toFixed(2)}
+                                        </div>
+
+                                        <FontAwesomeIcon icon={faTrashCan} />
+                                    </div>
+                                )
+                            }
+                        </div>
+
+                        <div className={styles.cartFooter}>
+                            <div className={styles.totalPrice}>Total price: ${cart.discountedTotal}</div>
+                            <div className={styles.totalProducts}>Total products: {cart.totalProducts}</div>
+                            <div className={styles.totalQuantity}>Total quantity: {cart.totalQuantity}</div>
+                        </div>
+                    </div>
+                )
+            }
+        </main>
     )
 }
 
