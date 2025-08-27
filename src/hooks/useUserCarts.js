e64ccae6102905437be45842cb81ef02c4ba75a7
Namig <namig.pashayev1@gmail.com>
2025-08-27 16:32:03 +0400

    Create update user cart  service and query

--- src/hooks/useUserCarts.js
+++ src/hooks/useUserCarts.js
@@ -1,8 +1,13 @@
-import { useQuery } from "@tanstack/react-query";
-import { getUserCarts } from "services/cartService";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { getUserCarts, updateUserCart } from "services/cartService";
 
 export const useUserCarts = (id) =>
     useQuery({
         queryKey: ['currentUser', 'carts'],
         queryFn: () => getUserCarts(id)
+    })
+
+export const useUpdateUserCart = () =>
+    useMutation({
+        mutationFn: (data) => updateUserCart(data)
     })
--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,7 +1,14 @@
 import apiClient from "./apiClient";
 
-
 export const getUserCarts = (id) =>
     apiClient
         .get(`/users/${id}/carts`)
+        .then(res => res.data)
+
+export const updateUserCart = ({ cartId, data }) =>
+    apiClient
+        .put(`/carts/${cartId}`, {
+            merge: true,
+            ...data
+        })
         .then(res => res.data)
