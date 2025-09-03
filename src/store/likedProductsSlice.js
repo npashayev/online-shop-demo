5893dacb906f5e9b5151abe7910c18777082a2c4
Namig <namig.pashayev1@gmail.com>
2025-09-03 09:34:45 +0400

    Add likedProductsSlice to redux toolkit

---
+++ src/store/likedProductsSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    byId: {},
+    allIds: []
+}
+
+const likedProductsSlice = createSlice({
+    name: "likedProducts",
+    initialState,
+    reducers: {
+        toggleLike: (state, action) => {
+            const product = action.payload
+            const id = product.id
+
+            if (state.byId[id]) {
+                delete state.byId[id]
+                state.allIds = state.allIds.filter(pid => pid !== id)
+            } else {
+                state.byId[id] = product;
+                state.allIds.push(id)
+            }
+        }
+    }
+})
+
+
+export const { toggleLike } = likedProductsSlice.actions;
+export const likedProductsReducer = likedProductsSlice.reducer;
--- src/store/store.js
+++ src/store/store.js
@@ -2,15 +2,17 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import userReducer from './userSlice';
+import { likedProductsReducer } from "./likedProductsSlice";
 
 const persistConfig = {
     key: "root",
     storage,
-    whitelist: ["user"] // only persist serializable user slice
+    whitelist: ["user", "likedProducts"] // only persist serializable user slice
 };
 
 const rootReducer = combineReducers({
-    user: userReducer
+    user: userReducer,
+    likedProducts: likedProductsReducer
 });
 
 // Persist only the reducer, never any hooks/functions
