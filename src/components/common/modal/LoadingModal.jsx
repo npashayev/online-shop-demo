acefff41bc447849765e7aff505300916614112b
Namig <namig.pashayev1@gmail.com>
2025-09-04 11:01:09 +0400

    Create LoadingModal component

---
+++ src/components/common/modal/LoadingModal.jsx
@@ -0,0 +1,33 @@
+import { useEffect } from 'react';
+import Loading from '../Loading'
+import styles from './modal.module.scss'
+
+const LoadingModal = ({ children }) => {
+
+    useEffect(() => {
+        document.body.style.overflow = 'hidden';
+
+        return () => {
+            document.body.style.overflow = '';
+        };
+    }, []);
+
+    return (
+        <div className={styles.main}>
+            <div
+                className={styles.overlay}>
+            </div>
+
+            <div className={styles.modal}>
+                <div className={styles.body}>
+                    {children}
+                </div>
+                <div>
+                    <Loading size='32px' />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LoadingModal
