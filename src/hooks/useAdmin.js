95eaa11496c616020b75cec9c3e5cca4ab8e9c6f
Namig <namig.pashayev1@gmail.com>
2025-09-04 15:45:44 +0400

    Remove unnecessary code

--- src/hooks/useAdmin.js
+++ src/hooks/useAdmin.js
@@ -4,5 +4,4 @@ import { deleteProduct } from "services/adminService"
 export const useDeleteProduct = () =>
     useMutation({
         mutationFn: (productId) => deleteProduct(productId),
-        onSuccess: (data) => console.log(data)
     }) 
