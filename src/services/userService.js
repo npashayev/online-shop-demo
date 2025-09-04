826f7ddf9ec21bd715f5bad4ad4c552a119d79e4
Namig <namig.pashayev1@gmail.com>
2025-09-04 15:43:42 +0400

    Add user role property to logged in user object in login service

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,9 +1,28 @@
 import apiClient from "./apiClient";
 
-export const login = (data) =>
-    apiClient
-        .post('/user/login', data)
-        .then(res => res.data)
+export const login = async (data) => {
+    try {
+        let res = await apiClient.post('/user/login', data)
+        let initialUser = res.data
+        console.log(initialUser)
+
+        // save only after everything succeeded
+        localStorage.setItem("accessToken", initialUser.accessToken)
+        localStorage.setItem("refreshToken", initialUser.refreshToken)
+
+        let fullUser = await getCurrentUser()
+        initialUser.role = fullUser.role
+
+        return initialUser
+
+    } catch (err) {
+        // cleanup in case first request succeeded but second failed
+        localStorage.removeItem("accessToken")
+        localStorage.removeItem("refreshToken")
+        throw err
+    }
+}
+
 
 export const register = (data) =>
     apiClient
