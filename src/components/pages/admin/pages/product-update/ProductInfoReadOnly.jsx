91396d52ae19ea96ecb075f6ae13f1adab70119f
Namig <namig.pashayev1@gmail.com>
2025-09-08 12:23:42 +0400

    Create ProductInfoReadOnly component to display the readonly info about product

---
+++ src/components/pages/admin/pages/product-update/ProductInfoReadOnly.jsx
@@ -0,0 +1,95 @@
+import styles from './product-info.module.scss'
+
+const InfoField = ({ label, value }) => (
+    <div className={styles.inputCnr}>
+        <label>{label}</label>
+        <p className={styles.info}>{value || ''}</p>
+    </div>
+);
+
+const ProductInfoReadOnly = ({ product, setIsEditMode }) => {
+    return (
+        <div className={styles.componentContainer}>
+            <div className={styles.buttonsCnr}>
+                <button
+                    onClick={() => setIsEditMode(true)}
+                    className={styles.editBtn}
+                >
+                    Edit
+                </button>
+            </div>
+
+            {/* General Information */}
+            <div className={styles.block}>
+                <div className={styles.heading}>General information</div>
+                <div className={styles.inputGroup}>
+                    <InfoField label="Title" value={product.title} />
+                    <InfoField label="Brand" value={product.brand} />
+                    <InfoField label="Category" value={product.category} />
+                </div>
+                <div className={styles.inputGroup}>
+                    <div className={styles.inputCnr}>
+                        <label>Description</label>
+                        <p className={`${styles.info} ${styles.descriptionInfo}`}>
+                            {product.description}
+                        </p>
+                    </div>
+                </div>
+
+                <div className={styles.inputGroup}>
+                    <InfoField label="Price" value={product.price} />
+                    <InfoField label="Discount percentage" value={product.discountPercentage} />
+                </div>
+
+                <div className={styles.inputGroup}>
+                    <InfoField label="Tags" value={product?.tags?.join(',')} />
+                </div>
+            </div>
+
+            {/* Physical Specifications */}
+            <div className={styles.block}>
+                <div className={styles.heading}>Physical Specifications</div>
+                <div className={styles.inputGroup}>
+                    <InfoField label="Weight" value={product.weight} />
+                    <InfoField label="Width" value={product.dimensions?.width} />
+                </div>
+
+                <div className={styles.inputGroup}>
+                    <InfoField label="Height" value={product.dimensions?.height} />
+                    <InfoField label="Depth" value={product.dimensions?.depth} />
+                </div>
+            </div>
+
+            {/* Additional Information */}
+            <div className={styles.block}>
+                <div className={styles.heading}>Additional information</div>
+                <div className={styles.inputGroup}>
+                    <InfoField label="Warranty information" value={product.warrantyInformation} />
+                    <InfoField label="Shipping information" value={product.shippingInformation} />
+                </div>
+
+                <div className={styles.inputGroup}>
+                    <InfoField label="Return policy" value={product.returnPolicy} />
+                    <InfoField label="Minimum order quantity" value={product.minimumOrderQuantity} />
+                </div>
+            </div>
+
+            <div className={styles.block}>
+                <div className={styles.heading}>Images</div>
+
+                <div className={styles.imageList}>
+                    {
+                        product.images.map((image, index) =>
+                            <div key={index} className={styles.imageCnr}>
+                                <img src={image} alt="image" className={styles.image} />
+                            </div>
+                        )
+                    }
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default ProductInfoReadOnly;
