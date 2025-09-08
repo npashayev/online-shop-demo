1b2af7aa58b20a7d311e066dce1b28d0892c2c4f
Namig <namig.pashayev1@gmail.com>
2025-09-08 15:41:22 +0400

    Create AddProductAction component

---
+++ src/components/pages/admin/components/AddProductAction.jsx
@@ -0,0 +1,19 @@
+import styles from './product-actions.module.scss'
+import RoleOnly from './RoleOnly'
+
+const AddProductAction = () => {
+    return (
+        <div className={styles.buttonsCnr}>
+            <RoleOnly roles={["admin", "moderator"]}>
+                <Link
+                    to={'/products/add-product'}
+                    className={`${styles.addBtn} ${styles.button}`}
+                >
+                    Add new product
+                </Link>
+            </RoleOnly>
+        </div >
+    )
+}
+
+export default AddProductAction
