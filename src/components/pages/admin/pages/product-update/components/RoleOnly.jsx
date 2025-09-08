095ea54c2da1c9a51817387c1ffd1c607c2dd877
Namig <namig.pashayev1@gmail.com>
2025-09-08 12:20:39 +0400

    Create RoleOnly component to allow only permitted roles to see the specified elements

---
+++ src/components/pages/admin/pages/product-update/components/RoleOnly.jsx
@@ -0,0 +1,10 @@
+import useAuth from "hooks/useAuth";
+
+const RoleOnly = ({ children, roles }) => {
+    const { user } = useAuth();
+
+    if (!user?.role || !roles.includes(user.role)) return null;
+    return children;
+};
+
+export default RoleOnly
