9f1dd3dd321df78a4a7f8f5fc58a9edd0f6e7b02
Namig <namig.pashayev1@gmail.com>
2025-09-11 15:20:10 +0400

    Update structure and styling for categories sidebar

--- src/components/pages/products/Sidebar.jsx
+++ src/components/pages/products/Sidebar.jsx
@@ -8,8 +8,6 @@ import { Link, useParams } from 'react-router-dom';
 
 
 const Sidebar = () => {
-
-
     const { productCategory } = useParams();
     const activeCategory = productCategory ?? 'all';
 
@@ -17,43 +15,41 @@ const Sidebar = () => {
 
     return (
         <aside className={styles.sidebar}>
-            <div className={styles.categoriesContainer}>
-                <div className={styles.categoriesHeading}>
-                    Categories
-                </div>
-                {
-                    isLoading
-                        ? <Loading size='20px' />
-                        : <div className={styles.categories}>
-                            <Link
-                                to={'/products'}
-                                className={
-                                    activeCategory === 'all'
-                                        ? `${styles.activeCategory} ${styles.category}`
-                                        : styles.category
-                                }
-                            >
-                                <FontAwesomeIcon icon={faChevronRight} size='xs' /> All
-                            </Link>
-
-                            {
-                                categories?.map(category =>
-                                    <Link
-                                        to={`/products/category/${category.slug}`}
-                                        key={category.slug}
-                                        className={
-                                            activeCategory === category.slug
-                                                ? `${styles.activeCategory} ${styles.category}`
-                                                : styles.category
-                                        }
-                                    >
-                                        <FontAwesomeIcon icon={faChevronRight} size='xs' /> {category.name}
-                                    </Link>
-                                )
-                            }
-                        </div>
-                }
+            <div className={styles.categoriesHeading}>
+                Categories
             </div>
+            {
+                isLoading
+                    ? <Loading style={{ fontSize: '20px', marginTop: '2rem' }} />
+                    : <div className={styles.categories}>
+                        <Link
+                            to={'/products'}
+                            className={
+                                activeCategory === 'all'
+                                    ? `${styles.activeCategory} ${styles.category}`
+                                    : styles.category
+                            }
+                        >
+                            <FontAwesomeIcon icon={faChevronRight} size='xs' /> All
+                        </Link>
+
+                        {
+                            categories?.map(category =>
+                                <Link
+                                    to={`/products/category/${category.slug}`}
+                                    key={category.slug}
+                                    className={
+                                        activeCategory === category.slug
+                                            ? `${styles.activeCategory} ${styles.category}`
+                                            : styles.category
+                                    }
+                                >
+                                    <FontAwesomeIcon icon={faChevronRight} size='xs' /> {category.name}
+                                </Link>
+                            )
+                        }
+                    </div>
+            }
         </aside>
     )
 }
