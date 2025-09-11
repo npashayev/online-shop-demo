063275df0fa2fcac6665c592579ebd4f6cebd2b1
Namig <namig.pashayev1@gmail.com>
2025-09-11 09:50:56 +0400

    Remove dropdown menu from LoginButton component and replace button tag with Link to navigate the user to login page

--- src/components/common/navbar/LoginMenu.jsx
+++ src/components/common/navbar/LoginMenu.jsx
@@ -3,33 +3,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRightToBracket } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
-const LoginMenu = ({ isOpen, setIsOpen, menuRef }) => {
-
+const LoginButton = ({ setIsOpen, menuRef }) => {
 
     return <div ref={menuRef} className={styles.main}>
-        <div className={`${styles.btnCnr} ${styles.loginBtnCnr}`} onClick={() => setIsOpen(prev => !prev)}>
+        <Link
+            to='/login'
+            onClick={() => setIsOpen(prev => !prev)}
+            className={`${styles.btn} ${styles.loginBtn}`}
+        >
             <FontAwesomeIcon icon={faRightToBracket} className={styles.loginButton} />
-        </div>
-
-        {
-            isOpen &&
-            <div className={styles.dropdown}>
-                <Link to="/login"
-                    onClick={() => setIsOpen(false)}
-                    className={styles.item}
-                >
-                    Login
-                </Link>
-
-                <Link to="/register"
-                    onClick={() => setIsOpen(false)}
-                    className={styles.item}
-                >
-                    Register
-                </Link>
-            </div>
-        }
+        </Link>
     </div>
 }
 
-export default LoginMenu
+export default LoginButton
