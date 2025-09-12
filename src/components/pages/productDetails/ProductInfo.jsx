3fcef1a46b5637bfe5dae8841be6128fb0332ed9
Namig <namig.pashayev1@gmail.com>
2025-09-12 16:33:44 +0400

    Create ProductInfoHeading component and separate it from ProductInfo

--- src/components/pages/productDetails/ProductInfo.jsx
+++ src/components/pages/productDetails/ProductInfo.jsx
@@ -1,66 +1,13 @@
 import AdditionalInfo from './AdditionalInfo';
 import styles from './product-info.module.scss'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
-import star from "/src/assets/star.png"
-import CartList from '../products/CartList';
-import { useState } from 'react';
-import LikeButton from 'components/common/products/LikeButton';
+
 
 const ProductInfo = ({ product }) => {
-    const [isCartListOpen, setIsCartListOpen] = useState(false)
-
-    const productRating = Math.round(product.rating * 10) / 10;
-    const discountPercentage = Math.floor(product.discountPercentage);
-    const newPrice = (product.price - (product.price * product.discountPercentage / 100)).toFixed(2)
 
     return (
         <>
             <div className={styles.infoCnr}>
-                <div className={styles.infoHeading}>
-                    <div className={styles.title}>
-                        {product.title}
-                    </div>
 
-                    <div className={styles.ratingCnr}>
-                        <div className={styles.starsCnr} style={{ width: `${(productRating / 5) * 90}px` }}>
-                            {[...Array(5)].map((_, i) => (
-                                <img key={i} src={star} alt="star" className={styles.star} />
-                            ))}
-                        </div>
-
-                        <div className={styles.ratingSeparator} />
-
-                        <div className={styles.reviewCount}>{product?.reviews?.length} reviews</div>
-                    </div>
-
-                    <div className={styles.priceCnr}>
-                        {
-                            discountPercentage > 0 &&
-                            <span className={styles.oldPrice}>
-                                {product.price}$
-                            </span>
-                        }
-                        <span className={styles.newPrice}>{newPrice}$</span>
-                    </div>
-                </div>
-
-                <div className={styles.buttonsCnr}>
-
-                    <LikeButton product={product} />
-
-                    <button onClick={() => setIsCartListOpen(prev => !prev)} className={styles.cartButton}>
-                        <FontAwesomeIcon icon={faCartShopping} className={styles.basketIcon} />
-                        Add to cart
-                    </button>
-
-                    {
-                        isCartListOpen &&
-                        <div className={styles.cartListCnr}>
-                            <CartList product={product} />
-                        </div>
-                    }
-                </div>
 
                 <div className={styles.separator} />
 
---
+++ src/components/pages/productDetails/ProductInfoHeading.jsx
@@ -0,0 +1,65 @@
+import styles from './product-info-heading.module.scss'
+import star from "/src/assets/star.png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import CartList from '../products/CartList';
+import LikeButton from 'components/common/products/LikeButton';
+import { useState } from 'react';
+
+
+const ProductInfoHeading = ({ product }) => {
+
+    const [isCartListOpen, setIsCartListOpen] = useState(false)
+
+    const productRating = Math.round(product.rating * 10) / 10;
+    const discountPercentage = Math.floor(product.discountPercentage);
+    const newPrice = (product.price - (product?.price * product.discountPercentage / 100)).toFixed(2)
+
+    return (
+        <div className={styles.infoHeading}>
+            <div className={styles.title}>
+                {product.title}
+            </div>
+
+            <div className={styles.ratingCnr}>
+                <div className={styles.starsCnr} style={{ width: `${(productRating / 5) * 90}px` }}>
+                    {[...Array(5)].map((_, i) => (
+                        <img key={i} src={star} alt="star" className={styles.star} />
+                    ))}
+                </div>
+
+                <div className={styles.ratingSeparator} />
+
+                <div className={styles.reviewCount}>{product?.reviews?.length} reviews</div>
+            </div>
+
+            <div className={styles.priceCnr}>
+                {
+                    discountPercentage > 0 &&
+                    <span className={styles.oldPrice}>
+                        {product.price}$
+                    </span>
+                }
+                <span className={styles.newPrice}>{newPrice}$</span>
+            </div>
+
+            <div className={styles.buttonsCnr}>
+                <LikeButton product={product} />
+
+                <button onClick={() => setIsCartListOpen(prev => !prev)} className={styles.cartButton}>
+                    <FontAwesomeIcon icon={faCartShopping} className={styles.basketIcon} />
+                    Add to cart
+                </button>
+
+                {
+                    isCartListOpen &&
+                    <div className={styles.cartListCnr}>
+                        <CartList product={product} />
+                    </div>
+                }
+            </div>
+        </div>
+    )
+}
+
+export default ProductInfoHeading
