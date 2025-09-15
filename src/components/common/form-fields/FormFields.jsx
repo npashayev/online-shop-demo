9976b53e16f84fa93c633d2848c5235144bcf404
Namig <namig.pashayev1@gmail.com>
2025-09-15 12:36:27 +0400

    Create common FormFields file for reusable input fields

---
+++ src/components/common/form-fields/FormFields.jsx
@@ -0,0 +1,19 @@
+import styles from "/src/styles/resource-form.module.scss";
+
+export const InfoField = ({ label, value }) => (
+    <div className={styles.inputCnr}>
+        <label>{label}</label>
+        <p className={styles.info}>{value || ''}</p>
+    </div>
+);
+
+export const RHFInput = ({ label, name, register, type = "text" }) => (
+    <div className={styles.inputCnr}>
+        <label>{label}</label>
+        <input
+            type={type}
+            {...register(name)}
+            className={styles.info}
+        />
+    </div>
+)
