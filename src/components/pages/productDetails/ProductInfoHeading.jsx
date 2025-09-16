b238eeaf108ef6d96c9636c23fb84dc635b34c1b
Namig <namig.pashayev1@gmail.com>
2025-09-16 09:07:26 +0400

    Remove unnecessary code

--- src/components/pages/productDetails/ProductInfoHeading.jsx
+++ src/components/pages/productDetails/ProductInfoHeading.jsx
@@ -2,14 +2,11 @@ import styles from './product-info-heading.module.scss'
 import star from "/src/assets/star.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
-import CartList from '../products/CartList';
+import AddToCartButton from '../products/AddToCartButton';
 import LikeButton from 'components/common/products/LikeButton';
-import { useState } from 'react';
 
 
 const ProductInfoHeading = ({ product }) => {
-
-    const [isCartListOpen, setIsCartListOpen] = useState(false)
 
     const productRating = Math.round(product.rating * 10) / 10;
     const discountPercentage = Math.floor(product.discountPercentage);
@@ -46,17 +43,7 @@ const ProductInfoHeading = ({ product }) => {
             <div className={styles.buttonsCnr}>
                 <LikeButton product={product} />
 
-                <button onClick={() => setIsCartListOpen(prev => !prev)} className={styles.cartButton}>
-                    <FontAwesomeIcon icon={faCartShopping} className={styles.basketIcon} />
-                    Add to cart
-                </button>
-
-                {
-                    isCartListOpen &&
-                    <div className={styles.cartListCnr}>
-                        <CartList product={product} />
-                    </div>
-                }
+                <AddToCartButton product={product} />
             </div>
         </div>
     )
