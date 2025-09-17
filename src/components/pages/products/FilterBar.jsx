43e770eb8bceb7010d089ac4d3e1e9ea1773ff20
Namig <namig.pashayev1@gmail.com>
2025-09-17 16:13:19 +0400

    Add categories open button for sidebar and improve responsiveness for filter bar

--- src/components/pages/products/FilterBar.jsx
+++ src/components/pages/products/FilterBar.jsx
@@ -1,10 +1,10 @@
-import styles from './filter-bar.module.scss'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import styles from './filter-bar.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faList, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import AddProductAction from '../admin/components/AddProductAction';
-import Select from 'react-select'
+import Select from 'react-select';
 
 const options = [
     { value: 'default', label: 'Default' },
@@ -12,31 +12,22 @@ const options = [
     { value: 'price-desc', label: 'Price Descending' },
     { value: 'rating-asc', label: 'Rating Ascending' },
     { value: 'rating-desc', label: 'Rating Descending' },
-]
+];
 
-const customStyles = {
-    control: (provided) => ({
-        ...provided,
-        width: "200px",
-        cursor: "pointer"
-    })
-};
-
-const FilterBar = ({ showSearchbar = true }) => {
+const FilterBar = ({ openCategories }) => {
 
     const [searchParams, setSearchParams] = useSearchParams();
     const [searchText, setSearchText] = useState('');
 
     const navigate = useNavigate();
 
-
     useEffect(() => {
         setSearchText(searchParams.get('q') || '')
-    }, [searchParams])
+    }, [searchParams]);
 
     const selectorValue = searchParams.get('sortBy') && searchParams.get('order')
         ? `${searchParams.get('sortBy')}-${searchParams.get('order')}`
-        : 'default'
+        : 'default';
 
     const selectedOption = options.find(opt => opt.value === selectorValue);
 
@@ -46,59 +37,65 @@ const FilterBar = ({ showSearchbar = true }) => {
         const trimmed = searchText.trim();
 
         if (!trimmed) {
-            navigate('/products')
-            return
+            navigate('/products');
+            return;
         }
 
         const newParams = new URLSearchParams(searchParams);
         newParams.set('q', trimmed);
-        navigate(`/products/search?${newParams.toString()}`)
-    }
+        navigate(`/products/search?${newParams.toString()}`);
+    };
 
     const handleOptionChange = (selectedOption) => {
         const { value } = selectedOption;
 
         setSearchParams(prev => {
-            const newParams = new URLSearchParams(prev)
+            const newParams = new URLSearchParams(prev);
 
             if (value == "default") {
-                newParams.delete('sortBy')
-                newParams.delete('order')
+                newParams.delete('sortBy');
+                newParams.delete('order');
             } else {
-                const [sortBy, order] = value.split('-')
-                newParams.set('sortBy', sortBy)
-                newParams.set('order', order)
+                const [sortBy, order] = value.split('-');
+                newParams.set('sortBy', sortBy);
+                newParams.set('order', order);
             }
-            return newParams
-        })
-    }
+            return newParams;
+        });
+    };
 
     return (
         <div className={styles.main}>
-            <Select
-                options={options}
-                isSearchable={false}
-                value={selectedOption}
-                onChange={handleOptionChange}
-                styles={customStyles}
-            />
+            <div className={styles.left}>
+                <button className={styles.categoryBtn} onClick={openCategories}>
+                    <FontAwesomeIcon className={styles.categoryIcon} icon={faList} />
+                </button>
+
+                <Select
+                    className={styles.selector}
+                    options={options}
+                    isSearchable={false}
+                    value={selectedOption}
+                    onChange={handleOptionChange}
+                />
+            </div>
 
             <div className={styles.right}>
-                {
-                    showSearchbar &&
-                    <form onSubmit={handleSearch} className={styles.form}>
-                        <input
-                            type="text"
-                            value={searchText}
-                            className={styles.searchBar}
-                            onChange={(e) => setSearchText(e.target.value)}
-                            placeholder='Search for products...'
+                <form onSubmit={handleSearch} className={styles.form}>
+                    <input
+                        className={styles.searchBar}
+                        type="text"
+                        value={searchText}
+                        onChange={(e) => setSearchText(e.target.value)}
+                        placeholder='Search for products...'
+                    />
+                    <button type='submit'>
+                        <FontAwesomeIcon
+                            className={styles.searchIcon}
+                            icon={faMagnifyingGlass}
                         />
-                        <button type='submit'>
-                            <FontAwesomeIcon icon={faMagnifyingGlass} className={styles.searchIcon} />
-                        </button>
-                    </form>
-                }
+                    </button>
+                </form>
 
                 <AddProductAction />
             </div>
