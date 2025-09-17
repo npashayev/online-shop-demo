16dda476fa0bed87dde96182247466b46534199a
Namig <namig.pashayev1@gmail.com>
2025-09-17 16:13:52 +0400

    Rename sidebar to categories

--- src/components/pages/products/Categories.jsx
+++ src/components/pages/products/Categories.jsx
@@ -1,34 +1,55 @@
 import { useCategories } from '../../../hooks/useProducts'
-import styles from './sidebar.module.scss'
+import styles from './categories.module.scss'
 import Loading from '../../common/Loading';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faChevronRight, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { Link, useParams } from 'react-router-dom';
+import { useEffect, useRef } from 'react';
 
 
 
-const Sidebar = () => {
+const Categories = ({ isCategoriesOpen, closeCategories }) => {
     const { productCategory } = useParams();
     const activeCategory = productCategory ?? 'all';
 
+    const categoriesRef = useRef(null);
+
     const { data: categories, isLoading } = useCategories();
 
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (categoriesRef.current && !categoriesRef.current.contains(event.target)) {
+                closeCategories()
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => document.removeEventListener("mousedown", handleClickOutside)
+    }, [])
+
     return (
-        <aside className={styles.sidebar}>
+        <aside
+            className={`${styles.sidebar} ${isCategoriesOpen ? styles.activeSidebar : ""}`}
+            ref={categoriesRef}
+        >
             <div className={styles.categoriesHeading}>
                 Categories
+                <button className={styles.closeBtn} onClick={closeCategories}>
+                    <FontAwesomeIcon icon={faXmark} className={styles.deleteIcon} />
+                </button>
             </div>
             {
                 isLoading
                     ? <Loading style={{ fontSize: '20px', marginTop: '2rem' }} />
                     : <div className={styles.categories}>
                         <Link
-                            to={'/products'}
                             className={
                                 activeCategory === 'all'
                                     ? `${styles.activeCategory} ${styles.category}`
                                     : styles.category
                             }
+                            to={'/products'}
+                            onClick={closeCategories}
                         >
                             <FontAwesomeIcon icon={faChevronRight} size='xs' /> All
                         </Link>
@@ -36,13 +57,14 @@ const Sidebar = () => {
                         {
                             categories?.map(category =>
                                 <Link
-                                    to={`/products/category/${category.slug}`}
-                                    key={category.slug}
                                     className={
                                         activeCategory === category.slug
                                             ? `${styles.activeCategory} ${styles.category}`
                                             : styles.category
                                     }
+                                    key={category.slug}
+                                    to={`/products/category/${category.slug}`}
+                                    onClick={closeCategories}
                                 >
                                     <FontAwesomeIcon icon={faChevronRight} size='xs' /> {category.name}
                                 </Link>
@@ -54,4 +76,4 @@ const Sidebar = () => {
     )
 }
 
-export default Sidebar
+export default Categories
