a95acc8e7d008f370ec5941bc6d3178dde53d2fb
Namig <namig.pashayev1@gmail.com>
2025-09-17 12:33:30 +0400

    Update useEnsureUserCart hook

--- src/hooks/useEnsureUserCart.js
+++ src/hooks/useEnsureUserCart.js
@@ -1,12 +1,14 @@
 import { useQueryClient } from "@tanstack/react-query";
+import { useToast } from "contexts/ToastContext";
 import { useAddNewUserCart } from "hooks/useUserCart";
 
 const useEnsureUserCart = () => {
     const queryClient = useQueryClient();
-    const addNewCart = useAddNewUserCart();
+    const { mutate, isPending, error } = useAddNewUserCart();
+    const { showToast } = useToast();
 
     const createCart = (userId) => {
-        addNewCart.mutate(
+        mutate(
             {
                 userId,
                 products: [{}], // create cart with an empty product placeholder
@@ -15,11 +17,12 @@ const useEnsureUserCart = () => {
                 onSuccess: (newUserCart) => {
                     queryClient.setQueryData(["currentUser", "cart"], newUserCart);
                 },
+                onError: () => showToast("Couldn't create new cart for you, please try again later", false)
             }
         );
     };
 
-    return { createCart };
+    return { createCart, isPending, error };
 };
 
 export default useEnsureUserCart;
