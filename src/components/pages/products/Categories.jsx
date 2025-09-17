2f13d9c3d493afc22246c7e78cb17b284d1b45cc
Namig <namig.pashayev1@gmail.com>
2025-09-17 16:33:47 +0400

    Create separete CategoriesContent for handling loading and error states for categories better

--- src/components/pages/products/Categories.jsx
+++ src/components/pages/products/Categories.jsx
@@ -1,31 +1,27 @@
-import { useCategories } from '../../../hooks/useProducts'
-import styles from './categories.module.scss'
-import Loading from '../../common/Loading';
+import styles from './categories.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronRight, faXmark } from '@fortawesome/free-solid-svg-icons';
-import { Link, useParams } from 'react-router-dom';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { useParams } from 'react-router-dom';
 import { useEffect, useRef } from 'react';
+import CategoriesContent from './CategoriesContent';
 
 
 
 const Categories = ({ isCategoriesOpen, closeCategories }) => {
     const { productCategory } = useParams();
     const activeCategory = productCategory ?? 'all';
-
     const categoriesRef = useRef(null);
-
-    const { data: categories, isLoading } = useCategories();
 
     useEffect(() => {
         const handleClickOutside = (event) => {
             if (categoriesRef.current && !categoriesRef.current.contains(event.target)) {
-                closeCategories()
+                closeCategories();
             }
-        }
+        };
 
         document.addEventListener("mousedown", handleClickOutside);
-        return () => document.removeEventListener("mousedown", handleClickOutside)
-    }, [])
+        return () => document.removeEventListener("mousedown", handleClickOutside);
+    }, []);
 
     return (
         <aside
@@ -38,42 +34,13 @@ const Categories = ({ isCategoriesOpen, closeCategories }) => {
                     <FontAwesomeIcon icon={faXmark} className={styles.deleteIcon} />
                 </button>
             </div>
-            {
-                isLoading
-                    ? <Loading style={{ fontSize: '20px', marginTop: '2rem' }} />
-                    : <div className={styles.categories}>
-                        <Link
-                            className={
-                                activeCategory === 'all'
-                                    ? `${styles.activeCategory} ${styles.category}`
-                                    : styles.category
-                            }
-                            to={'/products'}
-                            onClick={closeCategories}
-                        >
-                            <FontAwesomeIcon icon={faChevronRight} size='xs' /> All
-                        </Link>
 
-                        {
-                            categories?.map(category =>
-                                <Link
-                                    className={
-                                        activeCategory === category.slug
-                                            ? `${styles.activeCategory} ${styles.category}`
-                                            : styles.category
-                                    }
-                                    key={category.slug}
-                                    to={`/products/category/${category.slug}`}
-                                    onClick={closeCategories}
-                                >
-                                    <FontAwesomeIcon icon={faChevronRight} size='xs' /> {category.name}
-                                </Link>
-                            )
-                        }
-                    </div>
-            }
+            <CategoriesContent
+                activeCategory={activeCategory}
+                closeCategories={closeCategories}
+            />
         </aside>
     )
-}
+};
 
-export default Categories
+export default Categories;
---
+++ src/components/pages/products/CategoriesContent.jsx
@@ -0,0 +1,54 @@
+import { Link } from 'react-router-dom';
+import styles from './categories.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { useCategories } from 'hooks/useProducts';
+import Loading from 'components/common/Loading';
+
+const CategoriesContent = ({ activeCategory, closeCategories }) => {
+
+    const { data: categories, isLoading, error } = useCategories();
+
+    if (isLoading) return <Loading style={{ fontSize: '20px', marginTop: '2rem' }} />;
+
+    if (error) return (
+        <div className={styles.categories}>
+            Something went wrong while loading categories.
+        </div>
+    );
+
+    return (
+        <div className={styles.categories}>
+            <Link
+                className={
+                    activeCategory === 'all'
+                        ? `${styles.activeCategory} ${styles.category}`
+                        : styles.category
+                }
+                to={'/products'}
+                onClick={closeCategories}
+            >
+                <FontAwesomeIcon icon={faChevronRight} size='xs' /> All
+            </Link>
+
+            {
+                categories?.map(category =>
+                    <Link
+                        className={
+                            activeCategory === category.slug
+                                ? `${styles.activeCategory} ${styles.category}`
+                                : styles.category
+                        }
+                        key={category.slug}
+                        to={`/products/category/${category.slug}`}
+                        onClick={closeCategories}
+                    >
+                        <FontAwesomeIcon icon={faChevronRight} size='xs' /> {category.name}
+                    </Link>
+                )
+            }
+        </div>
+    )
+};
+
+export default CategoriesContent;
