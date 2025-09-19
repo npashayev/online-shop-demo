053d67613a37f2f049c8c0b36ff3e135768f0046
Namig <namig.pashayev1@gmail.com>
2025-09-19 11:01:06 +0400

    Update AddToCart functionality with new cart update logic

--- src/components/pages/products/AddToCartButton.jsx
+++ src/components/pages/products/AddToCartButton.jsx
@@ -1,82 +1,47 @@
-import { useQueryClient } from '@tanstack/react-query';
-import { useUpdateUserCart } from 'hooks/useUserCart'
 import React from 'react'
 import styles from './add-cart.module.scss'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useDispatch, useSelector } from 'react-redux';
 import { useToast } from 'contexts/ToastContext';
+import { addProduct, isProductExist } from 'store/cartSlice';
 
 const AddToCartButton = ({ product, style }) => {
 
-    const updateUserCart = useUpdateUserCart()
-    const queryClient = useQueryClient();
-    const cachedCart = queryClient.getQueryData(["currentUser", "cart"]);
-
     const { showToast } = useToast();
+    const dispatch = useDispatch();
+    const exists = useSelector(state => isProductExist(state, product))
 
-    const handleAddToCart = (event, product) => {
+    const handleAddToCart = (event) => {
         event.preventDefault();
         event.stopPropagation();
 
-        if (!cachedCart) showToast("You don't have a cart to add the product, please try again later", false)
-
-        const alreadyInCart = cachedCart?.products?.find(p => p.id === product.id);
-
-        if (alreadyInCart) {
-            alert("The product is already added to this cart!");
+        if (exists) {
+            showToast("This product is already added to this cart!", false);
             return;
         }
 
-        const updatedProductsArray = [...cachedCart.products, { id: product.id }];
-
-        updateUserCart.mutate({
-            cartId: cachedCart.id,
-            data: {
-                products: updatedProductsArray
-            },
-        },
-            {
-                onSuccess: (updatedCart) => {
-                    queryClient.setQueryData(["currentUser", "cart"], updatedCart)
-                },
-
-                onError: (error, updatedCart) => {
-                    if (updatedCart.cartId === 51) {
-                        const updatedProduct = {
-                            id: product.id,
-                            title: product.title,
-                            price: product.price,
-                            quantity: 1,
-                            discountPercentage: product.discountPercentage,
-                            thumbnail: product.thumbnail
-                        }
-                        queryClient.setQueryData(["currentUser", "cart"], cachedCart => ({
-                            ...cachedCart,
-                            products: [...cachedCart.products, updatedProduct]
-                        }))
-                    } else {
-                        showToast(error.message, false);
-                    }
-                }
-            }
-        )
+        dispatch(addProduct({
+            id: product.id,
+            title: product.title,
+            price: product.price,
+            quantity: 1,
+            discountPercentage: product.discountPercentage,
+            thumbnail: product.thumbnail
+        }))
+        showToast("Product added to the cart!")
     }
 
     return (
         <button
-            onClick={(event) => handleAddToCart(event, product)}
+            className={styles.addCartBtn}
             style={style}
-            className={styles.addCartBtn}>
-            {
-                updateUserCart.isPending
-                    ? "Adding to cart..."
-                    : <>
-                        <FontAwesomeIcon icon={faCartShopping} className={styles.basketIcon} />
-                        Add to cart
-                    </>
-            }
+            onClick={handleAddToCart}
+        >
+            <FontAwesomeIcon icon={faCartShopping} className={styles.basketIcon} />
+            Add to cart
         </button>
     )
 }
 
-export default AddToCartButton
+export default AddToCartButton;
