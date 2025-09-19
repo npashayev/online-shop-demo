47d3486808e7a9708a631954cdc1b4743363279c
Namig <namig.pashayev1@gmail.com>
2025-09-19 12:26:50 +0400

    Restructure LoginPage component by adding LoginForm as a child

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -1,105 +1,12 @@
-import { useState } from 'react';
-import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
+import LoginForm from 'components/common/login-form/LoginForm';
 import styles from './login.module.scss';
-import { useLogin } from '../../../hooks/useUser';
 import { useNavigate } from 'react-router-dom';
-import { useToast } from 'contexts/ToastContext';
 
 const LoginPage = () => {
-    const [passwordVisible, setPasswordVisible] = useState(false);
-    const [loginData, setLoginData] = useState({ username: '', password: '' });
-    const [errorMessage, setErrorMessage] = useState('');
-
-    const { mutate, error, reset, isPending } = useLogin();
     const navigate = useNavigate();
-    const { showToast } = useToast();
-
-    const handleInputChange = (e) => {
-        setErrorMessage("");
-        if (error) reset(); // clear previous API error
-        const { name, value } = e.target;
-        setLoginData(prev => ({
-            ...prev,
-            [name]: value
-        }));
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setErrorMessage('');
-        if (error) reset();
-        if (!loginData.username || !loginData.password) {
-            setErrorMessage('Please fill in all fields');
-            return
-        }
-
-        mutate(loginData, {
-            onSuccess: () => {
-                showToast("Successfully logged in!");
-                navigate('/');
-            },
-            onError: (error) => setErrorMessage(error.message)
-        });
-    }
-
     return (
         <main className={styles.page}>
-            <form
-                className={styles.form}
-                onSubmit={handleSubmit}
-            >
-                <h1 className={styles.formHeaderText}>
-                    Login
-                </h1>
-
-                {
-                    errorMessage &&
-                    <div className={styles.error}>
-                        {errorMessage}
-                    </div>
-                }
-
-                <div className={styles.inputField}>
-                    <input
-                        onChange={handleInputChange}
-                        value={loginData.username}
-                        type='text'
-                        name='username'
-                        disabled={isPending}
-                        placeholder=' '
-                        required
-                    />
-                    <label className={isPending ? styles.submitting : ''}>Username</label>
-                </div>
-
-                <div className={styles.inputField}>
-                    <input
-                        onChange={handleInputChange}
-                        value={loginData.password}
-                        name='password'
-                        required
-                        type={passwordVisible ? 'text' : 'password'}
-                        disabled={isPending}
-                        placeholder=' '
-                    />
-                    <label className={isPending ? styles.submitting : ''}>Password</label>
-
-                    <button
-                        className={styles.toggleBtn}
-                        type='button'
-                        disabled={isPending}
-                        onClick={() => setPasswordVisible(prev => !prev)}
-                    >
-                        {passwordVisible ? <FaRegEye /> : <FaRegEyeSlash />}
-                    </button>
-                </div>
-
-                <div className={styles.buttonCnr}>
-                    <button type="submit" disabled={isPending}>
-                        {isPending ? 'Logging in...' : 'Login'}
-                    </button>
-                </div>
-            </form>
+            <LoginForm navigate={() => navigate('/')} />
         </main>
     )
 }
