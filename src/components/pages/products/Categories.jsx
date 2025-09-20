c053f666bd548a4a117e1fd6f0feafc7c5129d1c
Namig <namig.pashayev1@gmail.com>
2025-09-20 17:30:22 +0400

    Display close button in categories sidebar only in small screen sizes

--- src/components/pages/products/Categories.jsx
+++ src/components/pages/products/Categories.jsx
@@ -2,7 +2,7 @@ import styles from './categories.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { useParams } from 'react-router-dom';
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 import CategoriesContent from './CategoriesContent';
 import useClickOutside from 'hooks/useClickOutside';
 
@@ -29,7 +29,7 @@ const Categories = ({ isCategoriesOpen, closeCategories }) => {
             <div className={styles.categoriesHeading}>
                 Categories
                 <button className={styles.closeBtn} onClick={closeCategories}>
-                    <FontAwesomeIcon icon={faXmark} className={styles.deleteIcon} />
+                    <FontAwesomeIcon icon={faXmark} />
                 </button>
             </div>
 
