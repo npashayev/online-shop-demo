3e2d59d0253f4b384ec95e4b4a411d77224dc6dd
Namig <namig.pashayev1@gmail.com>
2025-09-22 12:09:52 +0400

    Don't display login button in navbar if user is in login or register page

--- src/components/common/navbar/LoginButton.jsx
+++ src/components/common/navbar/LoginButton.jsx
@@ -1,14 +1,15 @@
 import styles from './user-options.module.scss'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRightToBracket } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const LoginButton = () => {
-
+    const location = useLocation();
     return (
-        <Link to='/login' className={`${styles.btn} ${styles.loginBtn}`}>
+        location.pathname !== '/login' && location.pathname !== '/register' &&
+        <Link Link to='/login' className={`${styles.btn} ${styles.loginBtn}`}>
             <FontAwesomeIcon icon={faRightToBracket} />
-        </Link>
+        </Link >
     )
 }
 
