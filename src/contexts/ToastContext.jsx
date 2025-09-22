16cf07e7d12d361016818057d6c0b79596348312
Namig <namig.pashayev1@gmail.com>
2025-09-22 10:45:47 +0400

    Update toast and create global index for toast

--- src/contexts/ToastContext.jsx
+++ src/contexts/ToastContext.jsx
@@ -1,27 +1,35 @@
 import Toast from "components/common/toast/Toast";
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useRef } from "react";
 
 const ToastContext = createContext();
 
 export function ToastProvider({ children }) {
     const [toast, setToast] = useState(null);
+    const timeoutRef = useRef(null);
 
     const showToast = (message, isSuccess = true) => {
         setToast({ message, isSuccess });
 
-        setTimeout(() => setToast(null), 5000);
-    }
+        // clear any previous timeout
+        if (timeoutRef.current) {
+            clearTimeout(timeoutRef.current);
+        }
+
+        // set new timeout
+        timeoutRef.current = setTimeout(() => {
+            setToast(null);
+            timeoutRef.current = null;
+        }, 5000);
+    };
 
     return (
         <ToastContext.Provider value={{ showToast }}>
             {children}
-
             {toast && <Toast message={toast.message} isSuccess={toast.isSuccess} />}
         </ToastContext.Provider>
-    )
+    );
 }
 
 export const useToast = () => useContext(ToastContext);
-
 
 export default ToastContext;
