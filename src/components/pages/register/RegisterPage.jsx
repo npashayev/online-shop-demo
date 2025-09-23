86e04571f456c69c46f7fce3de55476ffe7bbc19
Namig <namig.pashayev1@gmail.com>
2025-09-23 15:01:12 +0400

    Update information modal on register page

--- src/components/pages/register/RegisterPage.jsx
+++ src/components/pages/register/RegisterPage.jsx
@@ -1,17 +1,21 @@
 import InformationModal from "components/common/modal/InformationModal";
 import styles from 'components/common/login-register-page.module.scss';
 import RegisterForm from "./RegisterForm";
+import { useState } from "react";
 
 
 const RegisterPage = () => {
+    const [isModalOpen, setIsModalOpen] = useState(true);
+
     return (
         <main className={styles.page}>
-            <InformationModal>
-                Registering a new user will not actually add it to the server.
-                This will simulate a POST request and return the newly created user with a generated ID.
-                To log in, use a username and password from the dummyjson database.
-                After a successful registration, you will be redirected to the login page.
-            </InformationModal>
+            {
+                <InformationModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+                    This registration form is for demo purposes only.
+                    No real user account will be created. After submitting,
+                    you’ll be redirected to the login page and can log in with an existing user.
+                </InformationModal>
+            }
             <RegisterForm />
         </main>
     )
